34d7a8a43cba27b35207963f7c598a7b6553906f
rpeng <rpeng@44e3b816-bcf9-0310-ace2-eef029376a03>
2004-01-08 21:27:15 +0000

    tried to use import but it didn't work....
    git-svn-id: file:///home/rpeng/backups/repos/repository1/trunk/simpleboot@478 44e3b816-bcf9-0310-ace2-eef029376a03

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,6 @@
     ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
     ver <- as.character(ver)
     cat(paste("Simple Bootstrap Routines (version ", ver, ")\n", sep = ""))
-
     library(boot)
     library(modreg)
 }
