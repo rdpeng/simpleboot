ba66b7991c7fcf239a58e2403efa4c4c3942f961
rpeng <rpeng@44e3b816-bcf9-0310-ace2-eef029376a03>
2004-08-12 12:48:45 +0000

    Use .onAttach for banner message, .onLoad to check for boot package
    git-svn-id: file:///home/rpeng/backups/repos/repository1/trunk/simpleboot@2954 44e3b816-bcf9-0310-ace2-eef029376a03

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,11 @@
-.onLoad <- function(lib, pkg) {
+.onAttach <- function(lib, pkg) {
     where <- match(paste("package:", pkg, sep = ""), search())
     ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
     ver <- as.character(ver)
     cat(paste("Simple Bootstrap Routines (version ", ver, ")\n", sep = ""))
+}
+
+.onLoad <- function(lib, pkg) {
     if(!require(boot))
         stop(sQuote("boot"), " library required for ", sQuote("simpleboot"))
-    library(stats)
 }
