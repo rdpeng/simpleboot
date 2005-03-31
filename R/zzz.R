18dcead4156c915c83e0e041b18f3b060753ba7c
rpeng <rpeng@44e3b816-bcf9-0310-ace2-eef029376a03>
2005-03-31 18:22:54 +0000

    don't need where
    git-svn-id: file:///home/rpeng/backups/repos/repository1/trunk/simpleboot@4954 44e3b816-bcf9-0310-ace2-eef029376a03

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,4 @@
 .onAttach <- function(lib, pkg) {
-    where <- match(paste("package:", pkg, sep = ""), search())
     ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
     ver <- as.character(ver)
     cat(paste("Simple Bootstrap Routines (version ", ver, ")\n", sep = ""))
