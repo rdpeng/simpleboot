befe4b5579078b9dc4bee5343638d7215417e44d
Roger D. Peng [MBP 2018] <rdpeng@jhu.edu>
2019-01-28 16:41:55 -0500

    Don't print the date on attach

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(lib, pkg) {
     dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
-    msg <- gettextf("%s (%s %s)", dcf[, "Title"],
-                    as.character(dcf[, "Version"]), dcf[, "Date"])
+    msg <- gettextf("%s (%s)", dcf[, "Title"],
+                    as.character(dcf[, "Version"]))
     packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
 }
 
